7a027e0863f03c3f00f9c5966462e3da201f8704
Mario Carera <marcar76@gmail.com>
2022-03-08 22:07:28 -0300

    v1.0.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PerfilfotoComponent } from './components/perfilfoto/perfilfoto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    FormularioComponent,
+    ProyectosComponent,
+    SkillsComponent,
+    HeaderComponent,
+    PerfilfotoComponent
+    
   ],
   imports: [
     BrowserModule,
